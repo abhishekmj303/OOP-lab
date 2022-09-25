ca385e7e7e1179ee2ca369864a1c1c41eb002214
Abhishek M J <abhishekmj303@gmail.com>
2022-09-25 13:53:30 +0530

    Add kill-switch

--- auto_type.py
+++ auto_type.py
@@ -1,8 +1,9 @@
 import time
-from pynput.keyboard import Key, Controller
+import threading, _thread
+from pynput.keyboard import Key, Controller, Listener, HotKey
 
 # Complete path to the code
-file_path = "/home/abhishek/Programs/OOP/lab6/clinklist.cpp"
+file_path = "/home/abhishek/Programs/OOP/lab7/bank.cpp"
 
 # "\t" if indented using tab
 indent_string = "    "
@@ -11,25 +12,47 @@ indent_string = "    "
 initial_delay = 5
 
 # Delay between each letter while typing in seconds
-key_delay = 0.005
+key_delay = 0.1
 
-# TODO: Add kill switch (like ctrl+c)
+# Note: Kill the program from typing the code by pressing <alt>+c
+# Kill Switch
+switch = '<alt>+c'
 
-time.sleep(initial_delay)
-keyboard = Controller()
-with open(file_path, "r") as f:
-    code = f.read()
+def type_code():
+    keyboard = Controller()
+    with open(file_path, "r") as f:
+        code = f.read()
 
-    code = code.replace(indent_string, "")
-    count = code.count("{")
-    for a in code:
-        keyboard.type(a)
-        time.sleep(key_delay)
+        code = code.replace(indent_string, "")
+        count = code.count("{")
+        for a in code:
+            keyboard.type(a)
+            time.sleep(key_delay)
 
-    time.sleep(0.5)
-    with keyboard.pressed(Key.shift):
-        for i in range(count):
-            keyboard.press(Key.down)
-            keyboard.release(Key.down)
-    keyboard.press(Key.backspace)
-    keyboard.release(Key.backspace)
+        time.sleep(0.5)
+        with keyboard.pressed(Key.shift):
+            for i in range(count):
+                keyboard.press(Key.down)
+                keyboard.release(Key.down)
+        keyboard.press(Key.backspace)
+        keyboard.release(Key.backspace)
+
+def kill_switch():
+
+    def on_activate():
+        _thread.interrupt_main()
+        exit()
+
+    def for_canonical(f):
+        return lambda k: f(l.canonical(k))
+
+    hotkey = HotKey(HotKey.parse(switch), on_activate)
+    with Listener(
+            on_press=for_canonical(hotkey.press),
+            on_release=for_canonical(hotkey.release)) as l:
+        l.join()
+
+if __name__ == "__main__":
+    threading.Thread(target=kill_switch).start()
+    time.sleep(initial_delay)
+    type_code()
